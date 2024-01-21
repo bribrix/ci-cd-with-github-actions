904742aa86b7043a397c9d51b55afa5e9233bb9e
Bribrix <briac.marchandise@gmail.com>
2024-01-21 18:58:03 +0100

    update python-app.yml

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Define the GitHub repository address
+REPO_URL="https://github.com/bribrix/ci-cd-with-github-actions.git"
+
+# Define the directory to clone the repository
+CLONE_DIR="my_repo_deployment"
+
+export FLASK_APP=app.py
+export FLASK_ENV=development
+
+# Create a temporary folder to store the repository
+mkdir -p $CLONE_DIR
+
+# Clone the repository
+git clone $REPO_URL $CLONE_DIR
+
+# Change to the repository directory
+cd $CLONE_DIR
+
+# Install requirements
+pip install -r requirements.txt
+
+# Run the application
+nohup flask run &
+
+echo "Python executable: $(which python)"
+
+
+# Exiting the script
+exit 0
---
+++ test.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Define the GitHub repository address
+REPO_URL="https://github.com/bribrix/ci-cd-with-github-actions.git"
+
+# Define the directory to clone the repository
+CLONE_DIR="my_repo_temp"
+
+# Create a temporary folder to store the repository
+mkdir -p $CLONE_DIR
+
+# Clone the repository
+git clone $REPO_URL $CLONE_DIR
+
+# Change to the repository directory
+cd $CLONE_DIR
+
+# create a virtual environment
+python -m venv myenv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install requirements
+pip install -r requirements.txt
+
+# Run tests (assuming there's a script or command to run tests)
+# Replace 'run-tests-command' with the actual command to run tests
+
+# run the test
+
+python test-app.py
+
+sleep 5
+
+# deactivate the virtual environment
+deactivate
+
+# Cleaning up: delete the tmp folder
+cd ..
+rm -rf $CLONE_DIR
+
+# Exiting the script
+exit 0
